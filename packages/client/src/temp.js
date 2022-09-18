02a37e92d5547a47a45007b6cb136f32799552ee
Andy Willis <awillis@fastmail.fm>
2022-09-18 17:46:58 +0100

    jsconfig

---
+++ packages/client/src/temp.js
@@ -0,0 +1,11 @@
+/**
+ * Say message
+ * @param {string} message - message
+ * @returns {string}
+ */
+function saySomething(message) {
+  console.log(message);
+  return 'foo';
+}
+
+saySomething({});
